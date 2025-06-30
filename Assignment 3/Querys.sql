83d755551b1820383cfb4a04f3255012973156b0
Sergio Villaseñor <sergiovillasenorparra@gmail.com>
2025-06-30 05:39:27 +0200

    Assigment 3

--- Assignment 3/Querys.sql
+++ Assignment 3/Querys.sql
@@ -29,8 +29,7 @@ SELECT AVG(salario) FROM empleados;
 
 SELECT tiposproducto.nombre, COUNT(productos.nombre)
 FROM productos
-JOIN tiposproducto
-ON productos.tipoProductoId = tiposproducto.id
+JOIN tiposproducto ON productos.tipoProductoId = tiposproducto.id
 GROUP BY tipoProductoId;
 
 #3:
@@ -40,3 +39,15 @@ FROM Pedidos
 JOIN Clientes ON Pedidos.clienteId = Clientes.id 
 JOIN Usuarios ON Clientes.usuarioId = Usuarios.id;
 #NOTA: Cuidado con las asociaciones directas, hacen falta dos asociaciones porque la tabla pedidos nos da el IdCliente y luego desde Clientes tenemos que sacar el nombre en la tabla usuarios
+
+#4:
+
+SELECT usuarios.nombre, COUNT(DISTINCT productos.id)
+FROM lineaspedido
+JOIN productos ON lineaspedido.productoId = productos.id
+JOIN pedidos ON lineaspedido.pedidoId = pedidos.id
+JOIN clientes ON pedidos.clienteId = clientes.id
+JOIN usuarios ON clientes.usuarioId = usuarios.id
+GROUP BY usuarios.nombre
+ORDER BY COUNT(DISTINCT productos.id)
+LIMIT 1;
